e192739682719a6b444ea9c7869f8d56662cfde8
Ángel Ortega <codenode.ortega@gmail.com>
2024-01-25 16:22:03 -0600

    Add encryption and decryption functionality

---
+++ src/components/footer.js
@@ -0,0 +1,10 @@
+const Footer = Document.querySelector('footer');
+const FooterTemplate = `
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <p>Encriptador</p>
+            </div>
+        </div>
+    </div>
+`;  
